63da176d9f41c206412bf6f3a587921fadd70191
Raul Calero Garcia <raulcalerogarcia2003@gmail.com>
2024-02-24 19:20:54 +0100

    cambios

--- lib/screens/peluqueros_screen.dart
+++ lib/screens/peluqueros_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fl_peluqueria/screens/screens.dart';
+
 class PeluquerosScreen extends StatelessWidget {
    
   const PeluquerosScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: EasySearchBar(
+    title: Text('Example'),
+    onSearch: (value) => setState(() => searchValue = value)
+  ),
       body: Center(
          child: Text('PeluquerosScreen'),
       ),

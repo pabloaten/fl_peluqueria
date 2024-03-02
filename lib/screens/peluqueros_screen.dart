43dd8d549d8fd8e0b3fdcba7440624bf28dad138
Raul Calero Garcia <raulcalerogarcia2003@gmail.com>
2024-03-02 18:53:59 +0100

    Buscador funcional

--- lib/screens/peluqueros_screen.dart
+++ lib/screens/peluqueros_screen.dart
@@ -3,15 +3,24 @@ import 'package:fl_peluqueria/models/usuario.dart';
 import 'package:fl_peluqueria/services/usuarios_services.dart';
 import 'package:provider/provider.dart';
 import 'package:fl_peluqueria/screens/editar_usuario.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
 
-class PeluquerosScreen extends StatelessWidget {
+class PeluquerosScreen extends StatefulWidget {
   const PeluquerosScreen({Key? key}) : super(key: key);
 
   @override
+  State<PeluquerosScreen> createState() => _PeluquerosScreenState();
+}
+
+class _PeluquerosScreenState extends State<PeluquerosScreen> {
+
+  String searchValue = '';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: EasySearchBar(
         title: Text('Lista de Usuarios'),
+        onSearch: (value) => setState(() => searchValue = value),
       ),
       body: Consumer<UsuariosServices>(
         builder: (context, usuariosServices, _) {
@@ -20,10 +29,15 @@ class PeluquerosScreen extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           } else {
+            // Filtrar usuarios basados en el valor de búsqueda
+            final List<Usuario> filteredUsers = usuariosServices.usuarios.where((usuario) {
+              return usuario.nombreApellidos.toLowerCase().contains(searchValue.toLowerCase());
+            }).toList();
+            
             return ListView.builder(
-              itemCount: usuariosServices.usuarios.length,
+              itemCount: filteredUsers.length,
               itemBuilder: (context, index) {
-                final Usuario usuario = usuariosServices.usuarios[index];
+                final Usuario usuario = filteredUsers[index];
                 return ListTile(
                   leading: Icon(Icons.person),
                   title: Text(usuario.nombreApellidos),
@@ -45,3 +59,4 @@ class PeluquerosScreen extends StatelessWidget {
     );
   }
 }
+
